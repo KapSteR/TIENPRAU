1e72ef06031b53040887849ec977ab613f4a0c21
KapSteR <hr.kasper.nielsen@gmail.com>
2015-02-04 14:56:09 +0100

    Fix to load images

--- MATLAB/loadImages.m
+++ MATLAB/loadImages.m
@@ -3,13 +3,13 @@ function  [I] = loadImages(DATA_DIR, metaData, subID, seqID)
 % Load Images
 OLD_DIR =  cd([DATA_DIR, '\Images');
 
-cd(info(subID).name);
-cd(info(subID).sequences{seqID});
+cd(metaData(subID).name);
+cd(metaData(subID).sequences{seqID});
 
 files = dir;
 files = files(3:end);
 
-for frame = 1:info(subID).no_frames(seqID);
+for frame = 1:metaData(subID).no_frames(seqID);
     filename = files(frame).name;
     I{frame} = imread(filename);
 end
