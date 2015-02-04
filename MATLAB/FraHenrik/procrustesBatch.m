fe18ba6c5c7765e57d58c063b0848c0a6a913737
KapSteR <hr.kasper.nielsen@gmail.com>
2015-02-04 17:39:58 +0100

    First iteration of batch procrustes

--- MATLAB/FraHenrik/procrustesBatch.m
+++ MATLAB/FraHenrik/procrustesBatch.m
@@ -1,13 +1,20 @@
 function [Z, Z0 d] = procrustesBatch(S,S0)
 
+
+% If no reference shape is given
 if nargin == 1
 
-	S0 = S{1};
+	% Make reference shape the mean of all shapes
+	S0 = cellfun(@mean, S);
 
+	% Set number of iterations to two and adjust mean shape
+	% Two chosen on recomendation from "DTU ph.d web page"
 	n_iters = 2;
 
+% If reference shape is given
 elseif nargin == 2
 
+	% Only run alignment once
 	n_iters = 1;
 
 else
@@ -16,7 +23,14 @@ else
 end
 
 
+for i = 1:n_iters
 
+	[d, Z] = cellfun(@procrustes, S, S0, 'UniformOutput', false);
 
+	S = Z;
+
+end
+
+Z0 = cellfun(@mean, Z);
 
 end
