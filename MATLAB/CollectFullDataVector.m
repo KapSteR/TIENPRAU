ff34f7a8aa1f47692d964aec057fa34690a273e7
KapSteR <hr.kasper.nielsen@gmail.com>
2015-02-19 11:38:24 +0100

    More collection of data

--- MATLAB/CollectFullDataVector.m
+++ MATLAB/CollectFullDataVector.m
@@ -5,3 +5,41 @@ load('ProcessedData\g_warped_metaData.mat')
 
 load('ProcessedData\AllShapesAlign.mat');
 
+% load('ProcessedData\targetData.mat');
+
+dataVector = cell(nFrames,3);
+% target = cell(nFrames,1);
+
+remove = [];
+
+for i = 1:nFrames
+
+	if isempty(errorIdx(i))
+
+		dataVector{i,1} = g_warped{i};
+		dataVector{i,2} = Z(i,1,:);
+		dataVector{i,3} = Z(i,2,:);
+		% target{i} = targetData;
+
+	else
+		remove = [remove ; i];
+
+	end
+
+end
+
+% Remove cell with erroneous data;
+dataVector{remove,:} = []; 
+% target{remove} = [];
+
+
+dataMatrix = cell2mat(dataVector);
+% targetVector = cell2mat(target);
+
+save('ProcessedData\dataMatrix.mat', 'dataMatrix');
+% save('ProcessedData\targetVector', 'targetVector');
+
+
+
+
+
